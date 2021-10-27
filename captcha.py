3f2eed4b1ac440dc80145b276336ae719d55aa47
Xavier <59246000+ZiYang-xie@users.noreply.github.com>
2021-10-27 15:53:34 +0800

    增加验证码识别准确率

--- captcha.py
+++ captcha.py
@@ -3,9 +3,24 @@ import easyocr
 import io
 import numpy
 from PIL import Image
+from PIL import ImageEnhance
+
 
 def read_captcha(img_byte):
-    image = numpy.array(Image.open(io.BytesIO(img_byte)))
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(image, detail = 0)
-    return result[0]
+        img = Image.open(io.BytesIO(img_byte)).convert('L')
+        enh_bri = ImageEnhance.Brightness(img)
+        new_img = enh_bri.enhance(factor=1.5)
+        
+        image = numpy.array(new_img)
+        reader = easyocr.Reader(['en'])
+        horizontal_list, free_list = reader.detect(image, optimal_num_chars=4)
+        character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        allow_list = list(character)
+        allow_list.extend(list(character.lower()))
+    
+        result = reader.recognize(image, 
+                                allowlist=allow_list,
+                                horizontal_list=horizontal_list[0],
+                                free_list=free_list[0],
+                                detail = 0)
+        return result[0]
